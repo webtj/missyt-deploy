01b2d1d73f6d138e020cb3c5860d9d48d271563a
怕疼的猫 <taojian@petalmail.com>
2022-12-03 15:05:49 +0800

    增加操作命令

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 "use strict";
 const commander = require("commander");
-const { log, showLogo, checkUpdate } = require("./src/utils");
+const { showLogo, checkUpdate } = require("./src/utils");
 const initAction = require("./src/commands/init");
 const deployActionBefore = require("./src/commands/before");
+const { handleListAction, handleBackAction } = require("./src/commands/action");
 const pkg = require("./package.json");
 
 checkUpdate();
-commander.version(pkg.version).description(showLogo);
-
-commander.command("init").description("初始化部署配置").action(initAction);
+commander.version(pkg.version).description(showLogo(pkg.name));
+commander.command("init").description("初始化配置环境").action(initAction);
+commander.command("list").description("项目环境列表").action(handleListAction);
+commander.command("back").description("部署项目回退").action(handleBackAction);
 commander
   .command("run")
-  .option("-e, --env <env>", "部署环境: dev, test, prod, all")
-  .description("部署项目,[options] -e, --env <env> 部署环境: dev|test|prod|all")
+  .description("-e, --env <env> 待部署的环境")
+  .option("-e, --env <env>", "待部署环境")
   .action(deployActionBefore);
 //默认展示帮助信息
 if (process.argv && process.argv.length < 3) {
