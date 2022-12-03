f1c847d8033359bd01b8f25a6c1c27426ae49677
怕疼的猫 <taojian@petalmail.com>
2022-12-03 15:04:46 +0800

    增加部署前配置项检测

--- src/commands/before.js
+++ src/commands/before.js
@@ -1,39 +1,45 @@
+const { prompt } = require("inquirer");
 const { getConfig } = require("../config");
-const { log } = require("../utils");
-const inquirer = require("inquirer");
+const { log, infoColor } = require("../utils");
 
 module.exports = async function deployActionBefore(option) {
   const config = getConfig();
   if (Object.keys(config).length === 0) {
-    log.error(
-      "未检测到根目录下的deploy.config.js配置文件,请先执行：mdeploy init 初始化配置"
-    );
-    const answers = await inquirer.prompt([
+    log.error("✖️ 读取配置文件失败，请检查配置文件是否存在!");
+    const answers = await prompt([
       {
         type: "confirm",
         name: "init",
-        message: "是否立刻初始化配置文件？",
+        message: infoColor("是否需要初始化配置？"),
         default: true,
       },
     ]);
     if (answers.init) {
-      require("./init")();
+      await require("./init")();
     } else {
-      process.exit(1);
+      process.exit(0);
     }
   }
-  const deployEnvs = option.env || "all";
   const envList = config.envList;
   if (envList.length === 0) {
-    log.error("请配置envList，保证至少一个环境可用！");
-    process.exit(1);
+    log.error("™️ 请配置envList，保证至少一个环境可用！");
+    process.exit(0);
   }
-  let deployEnvList = envList.find((env) => env.name == deployEnvs);
-  if (!deployEnvList) {
-    log.warn("未找到对应的部署环境，将重新部署所有环境！");
+
+  const deployEnvs = option.env;
+  let evnToDeploy = envList.find((env) => env.value == deployEnvs);
+  if (!deployEnvs || !evnToDeploy) {
+    log.warn(`™️ 未找到部署环境${deployEnvs}！`);
+    const answers = await prompt([
+      {
+        type: "list",
+        name: "env",
+        message: infoColor("请选择部署环境："),
+        choices: envList,
+        default: envList[0].value,
+      },
+    ]);
+    evnToDeploy = envList.find((env) => env.value == answers.env);
   }
-  deployEnvList = deployEnvList ? [deployEnvList] : envList;
-  deployEnvList.forEach((env) => {
-    require("./deploy")(env);
-  });
+  await require("./deploy")(evnToDeploy);
 };
